50f01b8fe21a2f77d27734482ef15cff3ef2b416
HenriqueBraga <shenrique40moreira@gmail.com>
2023-08-24 17:06:57 -0300

    :sparkles: - completed the integration with the Free Dictionary API

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,59 +1,82 @@
-import useDictionaryAPI from '@/hooks/free-dictionary-API/free-dictionary.hook'
-import { Axios } from "axios";
-import { createContext, useState } from "react";
+import useDictionaryAPI, { type ApiResponse } from '@/hooks/free-dictionary-API/free-dictionary.hook'
+import { useState } from "react";
 import { BiSearch } from "react-icons/bi";
-
-interface meanings {
-  partOfSpeech: string
-  definitions: { definition: string, example: string, synonyms: string[], antonyms: string[] }[]
-}
-interface contextProps {
-  word: string
-  meanings: meanings[]
-}
-
-export const InputContext = createContext( {} as contextProps );
-
 
 
 export default function Main() {
 
+  const [dictionaryData, setDictionaryData] = useState<ApiResponse>()
   const [inputValue, setInputValue] = useState("")
   const DictionaryAPI = useDictionaryAPI()
 
-  function handleValue(e: any) {
+  function handleValue(e: React.ChangeEvent<HTMLInputElement>) {
     setInputValue(e.target.value)
   }
 
-  async function handleClick() {
+  async function handleClick(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
     const data = await DictionaryAPI.word(inputValue)
+    setDictionaryData(data)
 
     console.log(data)
   }
 
 
-
-  const value = {
-    inputValue, setInputValue
-  }
-
   return (
-    //<InputContext value={{}}>
     <main className=" w-screen flex flex-row justify-center items-center">
       <div className="h-[29.313rem] w-[39.625rem]">
-        <input type="text" name="search" id="search" placeholder="Try anything now" required onChange={handleValue} className="w-[39.625rem] h-16 p-5 bg-[var(--bg-theme)] rounded-lg text-[--text-color-theme] fixed" />
-        <span className="flex justify-end p-5">
-          <button type="submit" form="form1" className="z-[1]" onClick={handleClick}>
-            <BiSearch className="text-[#A745EC]" size={20} />
-          </button>
-        </span>
+        <form onSubmit={handleClick}>
+          <div className='w-[39.625rem] h-16 bg-[var(--bg-theme)] rounded-lg text-[--text-color-theme] flex items-center'>
+          <input className='flex-1 h-full p-5 bg-transparent' type="text" name="search" id="search" placeholder="Try anything now" value={inputValue} required onChange={handleValue} />
+            <button type="submit" className="z-[1] p-5 inline-block">
+              <BiSearch className="text-[#A745EC]" size={20} />
+            </button>
+          </div>
+        </form>
         <p className="text-3xl mt-11">{inputValue}</p>
-        <div className="flex flex-row items-center">
-          <p className="mt-11 mr-4">noun</p>
-          <hr className="w-[35.563rem] mt-11" />
-        </div>
+        <>
+        {
+          dictionaryData?.[0].meanings.slice(0, 1).map((meaning) => (
+            
+            <div className='flex flex-col' key={meaning.partOfSpeech}>
+              <div className="flex flex-row items-center">
+                <span className="mt-11 mr-4">{meaning.partOfSpeech}</span>
+                <hr className="w-[35.563rem] mt-11" />
+              </div>
+              <div className=' mt-7'>
+                <p>Meanings</p>
+                <ul className=' list-disc mt-6'>{meaning.definitions.slice(0, 3).map((definition) => (
+                  <li className=' marker:text-violet-500' key={definition.definition}>{definition.definition}</li>
+                  
+                ))}
+                </ul>
+              </div>
+              <div className=' mt-10 flex justify-between'>
+              Synonyms {''}
+                <p className=' space-x-7 text-violet-500'>
+                  
+                  {dictionaryData?.[0].meanings?.[0].synonyms?.map((synonyms, index) => (
+                    <span key={index}>
+                      {synonyms}                    
+                    </span>
+                  ))}
+                </p>
+              </div>
+            </div>
+            
+
+          ) ) 
+
+
+
+        }
+        {
+
+        }
+        </>
+        
       </div>
     </main>
-    //</InputContext>
   );
 }
--- src/hooks/free-dictionary-API/free-dictionary.hook.ts
+++ src/hooks/free-dictionary-API/free-dictionary.hook.ts
@@ -2,13 +2,47 @@ import Axios from "axios";
 
 const httpClient = Axios.create({baseURL: 'https://api.dictionaryapi.dev/api/v2/entries/en'})
 
+export type ApiResponse = {
+	word: string;
+	phonetic: string;
+	phonetics: Phonetic[];
+	meanings: Meaning[];
+	license: License;
+	sourceUrls: string[];
+}[];
+
+export type Phonetic = {
+	text: string;
+	audio: string;
+	sourceUrl: string;
+};
+
+export type Meaning = {
+	partOfSpeech: string;
+	definitions: Definition[];
+	synonyms: string[];
+	antonyms: string[];
+};
+
+export type Definition = {
+	definition: string;
+	synonyms: string[];
+	antonyms: string[];
+	example?: string;
+};
+
+export type License = {
+	name: string;
+	url: string;
+};
+
 
 export default function useDictionaryAPI() {
 
 
     async function word(word:string) {
         try {
-           const result = await httpClient.get(`/${word}`)
+           const result = await httpClient.get<ApiResponse>(`/${word}`)
            return result.data
         } catch (error) {
             console.error(error)
